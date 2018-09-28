b029106fb00def851f754826786ae80834b61db7
Atanu Mukherjee <mukherjeeatanu728@gmail.com>
2018-09-29 00:47:23 +0100

    Update the deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+set -x
 gitoperation()
 {
 Date=`date +%Y-%m-%d`
@@ -8,15 +9,58 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
 echo "My current Branch is" $branch
 
 if [ "$branch" == "$BranchName" ]; then
-echo "Already in Current branch"
+	echo "Already in Current branch"
 else
-git checkout Splunk_Release_$Date
-echo "switched"
+	git checkout Splunk_Release_$Date
+	echo "switched"
 fi
 git pull
 git diff master
 }
 
-ssh -o "StrictHostKeyChecking no" -i ~/.ssh/ssh_splunklive_rsa4k_201702  -l splunk ulvlfimw02 "cd /opt/splunk/splunk/etc;$(typeset -f gitoperation);gitoperation"
+checking_brach()
+		{
+			ssh -o "StrictHostKeyChecking no" -i ~/.ssh/ssh_splunklive_rsa4k_201702  -l splunk ulvlfimw02 "cd /opt/splunk/splunk/etc;$(typeset -f gitoperation);gitoperation"
+		}
 
-ssh -o "StrictHostKeyChecking no" -i ~/.ssh/ssh_splunklive_rsa4k_201702  -l splunk ulvlfisj01 " date; /opt/splunk/splunk/bin/splunk show shcluster-status -auth cogatamukherjee:Wednesday123; date"
+run_cluster_bundle()
+		{
+			ssh -o "StrictHostKeyChecking no" -i ~/.ssh/ssh_splunklive_rsa4k_201702  -l splunk ulvlfisj01 " date; /opt/splunk/splunk/bin/splunk show shcluster-status -auth cogatamukherjee:Wednesday123; date"
+		}
+
+
+show_menus() {
+                clear
+                echo "*********************"
+                                                      echo " M A I N - M E N U"
+                echo "*********************"
+                echo "1. Checking the correct Release branch"
+                echo "2. Run the Search head bundle on mw02"
+                echo "3. Restart the bundle on Searchhead sj01"
+                echo "4. Execute the bundle for Indexer on mw02"
+		echo "5. Restart the peers on mw02"
+                echo "6. Exit"
+                }
+
+
+read_options(){
+                #local choice
+                read -p "Enter choice [ 1 - 6] " choice
+                case $choice in
+                        1) checking_branch ;;
+                        2) run_cluster_bundle ;;
+                        3) restart_shbundle ;;
+                        4) exe_indexerbundle ;;
+			5) restart_peers;;
+                        6) exit 0;;
+                        *) echo -e "${RED}Error...${STD}" && sleep 2
+                esac
+                }
+
+tput clear
+                while true
+                do
+                show_menus
+                read_options
+                done
+
